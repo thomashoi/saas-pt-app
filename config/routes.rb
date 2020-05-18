858ff50c13c3b009f370cb872c2471d56e88b747
Thomas Hoi <thomashoi2005@gmail.com>
2020-05-18 21:13:50 +0800

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,32 @@
+Rails.application.routes.draw do
+
+  resources :user_projects
+  resources :artifacts
+  resources :tenants do
+    resources :projects do
+      get 'users', on: :member
+      put 'add_user', on: :member
+    end
+  end
+  resources :members
+  get 'home/index'
+
+   root :to => "home#index"
+
+
+  # *MUST* come *BEFORE* devise's definitions (below)
+  as :user do
+    match '/user/confirmation' => 'confirmations#update', :via => :put, :as => :update_user_confirmation
+  end
+
+  devise_for :users, :controllers => {
+    :registrations => "registrations",
+    :confirmations => "confirmations",
+    :sessions => "milia/sessions",
+    :passwords => "milia/passwords",
+  }
+
+  match '/plan/edit' => 'tenants#edit', via: :get, as: :edit_plan
+  match '/plan/update' => 'tenants#update', via: [:put, :patch], as: :update_plan
+
+end
